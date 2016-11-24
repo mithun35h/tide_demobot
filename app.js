ed318b2f56a5c819f8a7d9672a4045ebbb0ed755
Mithun H <mithun35h@gmail.com>
2016-11-24 18:23:42 -0500

    AppID and Password

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: null,
-    appPassword: null
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector, {
     localizerSettings: {
